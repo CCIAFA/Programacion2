edc7c000af82f26099104f9f6ffee8d6a5227496
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-05-01 09:54:30 -0300

    correccion de algunas funciones

--- TPPilas/pilasApuntadores.c
+++ TPPilas/pilasApuntadores.c
@@ -11,65 +11,68 @@ struct PilaRep {
 struct Nodo *tope;
 };
 
-Pila p_crear(){
-    Pila nueva_pila = (Pila) malloc(sizeof(struct PilaRep));
-    nueva_pila->valores=calloc((TAMANIO_MAXIMO+1),sizeof(TipoElemento));
-    nueva_pila->tope=0;
-    return nueva_pila;
+Pila p_crear() {
+Pila nueva_pila = (Pila) malloc(sizeof(struct PilaRep));
+nueva_pila->tope = NULL;
+return nueva_pila;
 }
-
-void p_apilar(Pila pila, TipoElemento elemento){
-    if(p_es_llena(pila)){
+void p_apilar (Pila pila, TipoElemento elemento) {
+struct Nodo *nuevo_nodo = malloc(sizeof(struct Nodo));
+if (p_es_llena(pila)) {
         return;
     }
-    pila->tope++;
-    pila->valores[pila->tope]=elemento;
+nuevo_nodo->datos = elemento;
+nuevo_nodo->siguiente = pila->tope;
+pila->tope = nuevo_nodo;
 }
 
-TipoElemento p_desapilar(Pila pila){
-    if(p_es_vacia(pila)){
+TipoElemento p_desapilar (Pila pila) {
+if (p_es_vacia(pila)) {
+        return;
+    }    
+struct Nodo *tope_actual = pila->tope;
+TipoElemento elemento = tope_actual->datos;
+pila->tope = tope_actual->siguiente;
+free(tope_actual);
+return elemento;
+}
+
+TipoElemento p_tope (Pila pila) {
+if (p_es_vacia(pila)) {
         return NULL;
-    }
-    TipoElemento elemento= pila->valores[pila->tope];
-    pila->tope--;
-    return elemento;
+    }       
+struct Nodo *tope_actual = pila->tope;
+return tope_actual->datos;
 }
 
-TipoElemento p_tope(Pila pila){
-    if(p_es_vacia(pila)){
-        return NULL;
-    }
-    return pila->valores[pila->tope];
+bool p_es_llena (Pila pila) {
+return(pila->tope==TAMANIO_MAXIMO);    
 }
 
-bool p_es_vacia(Pila pila){
-    return (pila->tope=0);
+bool p_es_vacia (Pila pila) {
+return pila->tope == NULL;
 }
 
-bool p_es_llena(Pila pila){
-    return(pila->tope==TAMANIO_MAXIMO);
-}
+void p_mostrar (Pila pila) {
 
-void p_mostrar(Pila pila){
-    if(p_es_vacia(pila)){
+if(p_es_vacia(pila)){
         printf("PILA VACIA \n");
         return;
     }
-    Pila Paux=p_crear();
-    TipoElemento X=te_crear(0);
-    printf("contenido de la pila:");
-    
-    // Recorro la pila desopilándola y pasándola al auxiliar
+
+Pila Paux = p_crear();
+TipoElemento X = crear_te(0);
+printf("Contenido de la pila: ");
+// Recorro la pila desopilándola y pasándola al auxiliar
 while (p_es_vacia(pila) != true) {
-    X = p_desapilar(pila);
-    printf("%d ", X->clave);
-    p_apilar(Paux, X);
+X = p_desapilar(pila);
+printf("%d ", X->clave);
+p_apilar(Paux, X);
 }
 // Recorro la pila auxiliar para pasarla a la original (o bien construyo la utilidad intercambiar)
 while (p_es_vacia(Paux) != true) {
-    X = p_desapilar(Paux);
-    p_apilar(pila, X);
+X = p_desapilar(Paux);
+p_apilar(pila, X);
 }
-
 printf("\n");
 }
