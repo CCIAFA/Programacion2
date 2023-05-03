d2cf29d15b3979114a43db4db96c71a913819052
nicognazzo <101167250+nicognazzo@users.noreply.github.com>
2023-05-03 17:51:52 -0300

    corregida identacion

--- TPPilas/punto2a.c
+++ TPPilas/punto2a.c
@@ -10,15 +10,15 @@ bool buscar_clave (Pila pila, int n){
 
     // ciclo que mueve los elementos a la pila auxiliar, similar a un p_intercambiar
 	while (!p_es_vacia(pila)) {
-	te = p_desapilar(pila);
-	p_apilar(Paux, te);
-	}
+		te = p_desapilar(pila);
+		p_apilar(Paux, te);
     
-	// condicional que se encontro el elemento buscado
-	if (te->clave == n){
-		resultado = True;
-		break;
-    }
+		// condicional que se encontro el elemento buscado
+		if (te->clave == n){
+			resultado = True;
+			break;
+    	}
+	}
 	
 	// recupero los elementos de la pila auxiliar a la original y la dejo como estaba
 	p_intercambiar (Pila pila, Pila Paux);
