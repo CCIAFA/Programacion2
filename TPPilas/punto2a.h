bce71f1cbe54fe7723e702f723549265a5a3604b
nicognazzo <101167250+nicognazzo@users.noreply.github.com>
2023-05-03 17:53:30 -0300

    agregadas librerias

--- TPPilas/punto2a.h
+++ TPPilas/punto2a.h
@@ -4,7 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include "pilas.h"
+#include "pilas.h"utilidadesPilas.h
+#include "utilidadesPilas.h"
+#include "Tipo_Elemento.h"
 
 bool buscar_clave (Pila pila, int n);
 
