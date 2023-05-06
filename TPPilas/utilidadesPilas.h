0cb6aa6030e8e8316a89a583fc67bb9c3bf669c4
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-05-06 16:07:29 -0300

    Update utilidadesPilas.h

--- TPPilas/utilidadesPilas.h
+++ TPPilas/utilidadesPilas.h
@@ -5,4 +5,7 @@
 void p_intercambiar(Pila pila,Pila Paux);
 void p_cargar(Pila pila, int cantidad);
 bool p_buscar_clave(Pila pila, int n);
+TipoElemento p_traer_clave (Pila pila, int n);
+void p_mostrar_todo(Pila pila);
+int p_longitud(Pila pila);
 #endif 
