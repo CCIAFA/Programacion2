8f4fe070d65c60f6831863f3143aed0a4d09a18f
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-05-06 15:08:59 -0300

    Add files via upload

---
+++ TPPilas/punto2f.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include "punto2f.h"
+#include "valida.h"
+#include "utilidadesPilas.h"
+#include "pilasArreglos.c"
+#include "Tipo_Elemento.c"
+#include "utilidadesPilas.c"
+#include "validaciones.c"
+
+
+int p_longituds(Pila pila){
+int i;
+Pila paux;
+paux=p_crear();
+i=0;
+TipoElemento x;
+while(!p_es_vacia(pila)){
+	x=p_desapilar(pila);
+	p_apilar(paux,x);
+	i=i+1;
+}
+p_intercambiar(pila,paux);
+return i;
+}
+
+int main() {
+    int clave,posicion,elemento,longitud;
+    Pila pila1;
+    pila1=p_crear();
+    ingresarNumero("\ningrese la cantidad de elementos que quiera cargar: ",&clave);
+    p_cargar(pila1,clave);
+    longitud=p_longituds(pila1);
+    printf("la pila tiene %d elementos",longitud);
+    
+}
---
+++ TPPilas/punto2f.h
@@ -0,0 +1,11 @@
+#ifndef Punto2
+#define Punto2
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include "pilas.h"
+
+Pila p_longituds(Pila pila);
+
+#endif //Punto2
