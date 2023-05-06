dedcfea8f3f6c8e013bf8db438411dbc9304ac8f
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-05-06 14:59:44 -0300

    Add files via upload

---
+++ TPPilas/punto2e.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include "punto2e.h"
+#include "utilidadesPilas.h"
+#include "valida.h"
+#include "pilasArreglos.c"
+#include "Tipo_Elemento.c"
+#include "utilidadesPilas.c"
+#include "validaciones.c"
+
+Pila p_duplicar(Pila pila){
+    TipoElemento clave;
+    int longitud;
+    longitud=p_longitud(pila);
+    Pila paux ,paux2;
+    paux=p_crear();
+    paux2=p_crear();
+    while(!p_es_vacia(pila)){
+        clave=p_desapilar(pila);
+        p_apilar(paux,clave);
+        p_apilar(paux2,clave);
+    }
+    p_intercambiar(pila,paux);
+    p_intercambiar(paux,paux2);
+    return paux;
+}
+
+
+int main() {
+    int clave,posicion,elemento,longitud;
+    Pila pila1,pres;
+    pila1=p_crear();
+    pres=p_crear();
+    ingresarNumero("\ningrese la cantidad de elementos que quiera cargar: ",&clave);
+    p_cargar(pila1,clave);
+
+    pres=p_duplicar(pila1);
+    printf("pila original:\n");
+    p_mostrar(pila1);
+    printf("pila duplicada:\n");
+    p_mostrar(pres);
+   
+    
+}
