83fbce505438d9d24211087bf95f9c84f816b9eb
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-05-06 16:03:17 -0300

    Add files via upload

---
+++ TPPilas/punto4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include "punto2f.h"
+#include "valida.h"
+#include "pilasArreglos.c"
+#include "Tipo_Elemento.c"
+#include "utilidadesPilas.c"
+#include "validaciones.c"
+
+char* pasar_base(int n,int b){
+    char* numero=(char*)calloc(30,1);
+    Pila pila=p_crear();
+    TipoElemento x;
+    int resto;
+    int i=0;
+    while(n>0){
+        resto=n % b;
+        n=n/b;
+        x=te_crear(resto);
+        p_apilar(pila,x);
+    }
+    while(!p_es_vacia(pila)){
+        x=p_desapilar(pila);
+        if(x->clave<=9){
+            *(numero+i)=48+x->clave;
+        }else{
+           *(numero+i)=55+x->clave; 
+        }
+        i++;
+    }
+    return numero;
+}
+
+int main() {
+    int numero,base;
+    char* resultado;
+    ingresarNumero("\ningrese el numero: ",&numero);
+    ingresarNumero("\ningrese la base: ",&base);
+    resultado=pasar_base(numero,base);
+    printf("numero convertido: %s",resultado);
+    printf("complejidad o(n)");
+}
---
+++ TPPilas/punto4.h
@@ -0,0 +1,11 @@
+#ifndef Punto4
+#define Punto4
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include "pilas.h"
+
+char* pasar_base(int n,int b);
+
+#endif //Punto4
