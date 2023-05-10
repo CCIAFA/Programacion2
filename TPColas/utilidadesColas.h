145b7a5b71c1badae635c31f7120137a545ea22e
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-05-10 15:42:36 -0300

    Update utilidadesColas.h

--- TPColas/utilidadesColas.h
+++ TPColas/utilidadesColas.h
@@ -4,5 +4,6 @@
 #include "colas.h"
 void p_intercambiar(Cola cola,Cola Caux);
 void p_cargar(Cola cola, int cantidad);
-
+int c_longitud(Cola cola);
+bool c_buscar_clave (Cola cola, int n);
 #endif 
