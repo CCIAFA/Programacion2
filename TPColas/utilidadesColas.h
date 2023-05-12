690ba02582cffe5004dfd8964d132b8fffc0b3c7
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-05-11 22:11:06 -0300

    Update utilidadesColas.h

--- TPColas/utilidadesColas.h
+++ TPColas/utilidadesColas.h
@@ -2,9 +2,12 @@
 #define UTILIDADESC_H
 #include <stdbool.h>
 #include "colas.h"
-void p_intercambiar(Cola cola,Cola Caux);
-void p_cargar(Cola cola, int cantidad);
+void c_intercambiar(Cola cola,Cola Caux);
+void c_cargar(Cola cola, int cantidad);
 int c_longitud(Cola cola);
 bool c_buscar_clave (Cola cola, int n);
+int c_buscar_posicion (Cola cola, int n);
+Cola c_duplicar(Cola cola);
+bool c_tiene_repetidos(Cola cola);
 void c_cargar_no_repetidos(Cola cola, int cantidad);
 #endif 
