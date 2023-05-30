f258869fdc5ba154adc1f0ffdb44d5320edc055e
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-05-29 21:07:35 -0300

    Update arbol-binario-busqueda.h

--- TPArboles/arbol-binario-busqueda.h
+++ TPArboles/arbol-binario-busqueda.h
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "nodo.h"
-#include "tipo_elemento.h"
+#include "Tipo_Elemento.h"
 
 struct ArbolBinarioBusquedaRep;
 typedef struct ArbolBinarioBusquedaRep *ArbolBinarioBusqueda;
