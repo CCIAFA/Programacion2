b398126e79ecbd002da11a402cfa5ed6a278d3ec
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-05-31 16:55:28 -0300

    Update utilidadesArboles.h

--- TPArboles/utilidadesArboles.h
+++ TPArboles/utilidadesArboles.h
@@ -2,9 +2,11 @@
 #define UTILIDADESA_H
 #include <stdbool.h>
 #include "arbol-binario.h"
+#include "arbol-avl.h"
 
 void in_orden(NodoArbol N);
 int a_altura(ArbolBinario a);
+int avl_altura(ArbolAVL avl);
 NodoArbol buscar_nodo(NodoArbol nodo, int clave);
 void cargar_arbol(ArbolBinario a);
 
