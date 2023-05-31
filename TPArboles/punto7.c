7a79893425ab10a221e807d32ec4053dc2795640
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-05-31 14:59:09 -0300

    Update punto7.c

--- TPArboles/punto7.c
+++ TPArboles/punto7.c
@@ -5,12 +5,6 @@
 #include <ctype.h>
 #include "punto4.h"
 #include "valida.h"
-#include "arbol-binario.c"
-#include "Tipo_Elemento.c"
-#include "nodo.c"
-#include "utilidadesArboles.c"
-#include "validaciones.c"
-
 
 bool a_equivalente_int(NodoArbol nodo1, NodoArbol nodo2) {
     int x1,x2;
@@ -43,26 +37,13 @@ int main() {
     TipoElemento x;
     NodoArbol n;
     bool res;
-    x=te_crear(10);
-    a=a_crear();
-    a_establecer_raiz(a,x);
-    x=te_crear(20);
-    n=a_conectar_hi(a,a_raiz(a),x);
-    x=te_crear(25);
-    a_conectar_hd(a,n,x);
-    x=te_crear(5);
-    n=a_conectar_hi(a,n,x);
     
-    x=te_crear(10);
+    a=a_crear();
     a2=a_crear();
-    a_establecer_raiz(a2,x);
-    x=te_crear(20);
-    n=a_conectar_hi(a2,a_raiz(a2),x);
-    x=te_crear(25);
-    a_conectar_hd(a2,n,x);
-    x=te_crear(5);
-    n=a_conectar_hi(a2,n,x);
-
+    printf("carga de arbol1:\n");
+    cargar_arbol(a);
+    printf("carga de arbol2:\n");
+    cargar_arbol(a2);
 res=a_equivalentes(a,a2);
 if(res){printf("son equivalentes");}else{printf("no son eqivalentes");}
 
