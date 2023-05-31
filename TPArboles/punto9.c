4f35ddbfa0b3787f57c2f657e4b734a36a4aaef9
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-05-31 16:56:56 -0300

    Add files via upload

---
+++ TPArboles/punto9.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include "punto9.h"
+
+#include "arbol-binario.c"
+#include "arbol-avl.c"
+#include "Tipo_Elemento.c"
+#include "nodo.c"
+#include "utilidadesArboles.c"
+#include "validaciones.c"
+
+
+void copiar_arbol_avl(NodoArbol nodo, ArbolAVL avl) {
+    if (nodo == NULL) {
+        return;
+    }
+
+    avl_insertar(avl, nodo->datos);
+    copiar_arbol_avl(n_hijoizquierdo(nodo), avl);
+    copiar_arbol_avl(n_hijoderecho(nodo), avl);
+}
+
+ArbolAVL cargar_avl(NodoArbol nodo) {
+    ArbolAVL avl = avl_crear();
+
+    copiar_arbol_avl(nodo, avl);
+
+    return avl;
+}
+
+int main() {
+    ArbolBinario a;
+    NodoArbol n;
+    ArbolAVL avl;
+    int i,clave;
+    a=a_crear();
+    cargar_arbol(a);
+    printf("inorder arbol binario:\n");
+    in_orden(a_raiz(a));
+    i=a_altura(a);
+    printf("\nla altura del arbol binario es %d\n",i);
+    avl=cargar_avl(a_raiz(a));
+    printf("inorder arbol avl\n");
+    in_orden(avl_raiz(avl));
+    i=avl_altura(avl);
+    printf("\nla altura del arbol avl es %d\n",i);
+    printf("la complejidad es O(log n) por el avl_insertar");
+}
---
+++ TPArboles/punto9.h
@@ -0,0 +1,9 @@
+#ifndef punto9_H
+#define punto9_H
+#include <stdbool.h>
+#include "arbol-binario.h"
+#include "arbol-avl.h"
+#include "utilidadesArboles.h"
+
+ArbolAVL cargar_avl(NodoArbol nodo);
+#endif 
