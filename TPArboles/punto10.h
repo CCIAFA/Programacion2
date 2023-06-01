2f02bde72689a63400abbf844978ae05c70f9ce8
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-05-31 21:00:47 -0300

    Update punto10.h

--- TPArboles/punto10.h
+++ TPArboles/punto10.h
@@ -1,12 +1,9 @@
-#ifndef punto9_10
-#define punto9_11
+#ifndef punto10_H
+#define punto10_H
 #include <stdbool.h>
 #include "arbol-binario-busqueda.h"
 #include "arbol-avl.h"
 #include "utilidadesArboles.h"
 
-void cargarSeriesEnArboles(int n); 
+void cargarSeriesEnArboles(int n,int rango_maximo,int tamanio);
 #endif 
-
-
-
