6f06f358a800c7a0d6b8b7b3c0c89a56e9800869
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-06-12 22:36:57 -0300

    Update punto6.c

--- TPHash/punto6.c
+++ TPHash/punto6.c
@@ -1,91 +1,200 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include "tabla_hash.h"
 #include "valida.h"
 #include "punto6.h"
 
-#define TAMANO_TABLA 50
+#define TAMANO_TABLA 20
 
-void main(){
-    int opcion;
-	do
-	{
-		opcion = menu();
-		
-		switch (opcion)
-		{
-			case '1':
-				system("cls");
-				cargar();
-				printf("\n\n");
-				system("Pause");
-				break;
-			case '2':
-				system("cls");
-				mostrar();
-				printf("\n\n");
-				system("Pause");
-				break;
-			case '0':
-				break;
-			default:
-				printf("\nOpcion incorrecta!");
-				printf("\nPresione [ENTER] para continuar");
-				getchar();
-                fflush(stdin);
-				break;
-		}
-	} while (opcion != '0');
-	
-	printf("\n\nPrograma Finalizado.\n\n");
-	system("pause");
-	return 0;
-}
-int menu()
-{
-	system("cls");
-	printf("---------------------------------------------------------------------");	
-	printf("\n Registro vacunados por COVID ");
-	printf("\n---------------------------------------------------------------------");
-	printf("\n 1 - Cargar datos");
-	printf("\n 2 - Mostrar datos");
-	printf("\n\n 0 - Salir");
-	printf("\n---------------------------------------------------------------------");
-	printf("\n Ingrese una opcion: ");
-		
-	return getchar();
-}
-void cargar(){
-    Fecha fecha = cargarFecha();
-    Persona persona = cargarPersona();
-}
-
-Persona cargarPersona() {
-    Persona persona;
-    bool dniCorrecto=false;
-    while(!dniCorrecto){
-        ingresarNumero("Ingrese el DNI valido: ", &persona.dni);
-        dniCorrecto = validarNumero8Digitos(persona.dni); 
+bool esFechaValida(int dia, int mes, int anio) {
+    if (anio < 2020 || anio > 2023 || mes < 1 || mes > 12 || dia < 1) {
+        return false;
     }
 
+    int diasEnMes;
+    switch (mes) {
+        case 2: // Febrero
+            if ((anio % 4 == 0 && anio % 100 != 0) || anio % 400 == 0) {
+                diasEnMes = 29;
+            } else {
+                diasEnMes = 28;
+            }
+            break;
+        case 4: // Abril
+        case 6: // Junio
+        case 9: // Septiembre
+        case 11: // Noviembre
+            diasEnMes = 30;
+            break;
+        default:
+            diasEnMes = 31;
+            break;
+    }
+
+    return dia <= diasEnMes;
+}
+
+int obtenerNumerosFecha(const char *fecha) {
+    char numeros[11];
+    int indiceNumeros = 0;
+
+    for (int i = 0; fecha[i] != '\0'; i++) {
+        if (fecha[i] >= '0' && fecha[i] <= '9') {
+            numeros[indiceNumeros] = fecha[i];
+            indiceNumeros++;
+        }
+    }
+
+    numeros[indiceNumeros] = '\0';
+    int numerosFecha = atoi(numeros);
+
+    return numerosFecha;
+}
+
+int funcionHash(int fecha) {
+    int suma = 0;
+    while (fecha > 0) {
+        suma += fecha % 10;
+        fecha /= 10;
+    }
+    return suma % TAMANO_TABLA;
+}
+
+// Función para mostrar los datos de un paciente
+void mostrarPacientesEnFecha(vacunado pacientes[], int cantidad) {
+    printf("Pacientes:\n");
+    printf("----------------------------\n");
+    
+    for (int i = 0; i < cantidad; i++) {
+        printf("DNI: %d\n", pacientes[i].dni);
+        printf("Apellido: %s\n", pacientes[i].apellido);
+        printf("Nombres: %s\n", pacientes[i].nombre);
+        printf("----------------------------\n");
+    }
+}
+
+// Función para cargar manualmente los datos de un paciente
+vacunado cargarPaciente() {
+    vacunado paciente;
+    bool fechaCorrecta =false;
+    int fechaNum;
+    bool dniCorrecto=false;
+
+    while (!fechaCorrecta)
+    {
+        printf("Ingrese la fecha de vacunacion (dd/mm/aaaa): ");
+        scanf("%s", paciente.fecha);   
+        fechaNum = obtenerNumerosFecha(paciente.fecha);
+
+        int dia = fechaNum / 1000000;
+        int mes = (fechaNum / 10000) % 100;
+        int anio = fechaNum % 10000;
+        fechaCorrecta = esFechaValida(dia, mes, anio);
+
+        if (!fechaCorrecta) {
+            printf("Fecha invalida. Por favor, ingrese una fecha valida.\n");
+        }
+    }
+    
+    while(!dniCorrecto){
+        ingresarNumero("Ingrese el DNI: ", &paciente.dni);
+        dniCorrecto = validarNumero8Digitos(paciente.dni); 
+    }
     printf("Ingrese el apellido: ");
-    scanf("%s", persona.apellido);
-    printf("Ingrese el nombre: ");
-    scanf("%s", persona.nombre);
-    return persona;
+    scanf("%s", paciente.apellido);
+    printf("Ingrese los nombres: ");
+    scanf("%s", paciente.nombre);
+    return paciente;
 }
 
-void mostrarPersona(Persona persona) {
-    printf("DNI: %d\n", persona.dni);
-    printf("Apellido: %s\n", persona.apellido);
-    printf("Nombre: %s\n", persona.nombre);
-    printf("----------------------------\n");
+int main() {
+    // Crear la tabla hash con el tamaño y la función de hash especificados
+    TablaHash tabla = th_crear(TAMANO_TABLA, funcionHash);
+
+    // Abrir el archivo de datos de pacientes en modo de adición binaria
+    FILE *archivo = fopen("vacunados.dat", "rb+");
+
+    // Validar si el archivo se abrió correctamente
+    if (archivo == NULL) {
+        printf("Error al abrir el archivo.\n");
+        return 1;
+    }
+
+    char opcion;
+    do {
+        // Cargar los datos de un paciente
+        vacunado paciente = cargarPaciente();
+        int fechaNumeros;
+        
+        // Guardar el paciente en el archivo
+        fwrite(&paciente, sizeof(vacunado), 1, archivo);
+
+        // Crear un TipoElemento con la clave (fecha) y la posición en el archivo como valor
+        fechaNumeros = obtenerNumerosFecha(paciente.fecha);
+        TipoElemento te = te_crear(fechaNumeros);
+        te->valor = (int*)(ftell(archivo) - sizeof(vacunado));
+
+        // Insertar el TipoElemento en la tabla hash
+        th_insertar(tabla, te);
+
+        printf("¿Desea cargar otro paciente? (s/n): ");
+        scanf(" %c", &opcion);
+    } while (opcion == 's' || opcion == 'S');
+
+    // Cerrar el archivo
+    fclose(archivo);
+
+    // Mostrar los elementos de la tabla hash
+    th_mostrar(tabla);
+
+    char fecha[11];
+    int fechaNumeros;
+    
+    printf("Ingrese una fecha para realizar una consulta (0 para salir): ");
+    scanf("%s", fecha);
+
+    while (strcmp(fecha, "0") != 0) {
+        fechaNumeros = obtenerNumerosFecha(fecha);
+        TipoElemento te = th_recuperar(tabla, fechaNumeros);
+
+        if (te != NULL) {
+            archivo = fopen("vacunados.dat", "rb");
+            if (archivo != NULL) {
+                // Crear un arreglo para almacenar los pacientes
+                vacunado pacientes[TAMANO_TABLA];
+                int cantidad = 0;
+
+                while (!feof(archivo)) {
+                // Leer un paciente del archivo
+                 vacunado paciente;
+        fread(&paciente, sizeof(vacunado), 1, archivo);
+
+            if (obtenerNumerosFecha(paciente.fecha) == fechaNumeros) {
+            // El paciente coincide con la fecha buscada, almacenarlo en el array
+            pacientes[cantidad] = paciente;
+            cantidad++;
+            }
+        }   
+
+    fclose(archivo);
+
+    if (cantidad > 0) {
+        mostrarPacientesEnFecha(pacientes, cantidad);
+    } else {
+        printf("No se encontraron pacientes en la fecha ingresada.\n");
+}
+        } else {
+            printf("Error al abrir el archivo.\n");
+        }
+    } else {
+        printf("La fecha ingresada no se encontró en la tabla hash.\n");
+    }
+
+    printf("Ingrese otra fecha (0 para salir): ");
+    scanf("%s", fecha);
 }
 
-void mostrarFecha(Fecha fecha) {
-    printf("\n%d/", fecha.dia);
-    printf("%d/", fecha.mes);
-    printf("%d \n", fecha.anio);
-    printf("----------------------------\n");
+    return 0;
 }
