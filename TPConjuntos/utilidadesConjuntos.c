96713fe573e7f9b243db9781e6908deddade5758
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-06-18 17:49:14 -0300

    Update utilidadesConjuntos.c

--- TPConjuntos/utilidadesConjuntos.c
+++ TPConjuntos/utilidadesConjuntos.c
@@ -19,3 +19,27 @@ void cto_cargar(Conjunto Conjunto, int cantidad){
 		printf("no se puede cargar mas elementos que el tamaño maximo\n");
 	}
 }
+
+void cto_cargar_no_repetidos(Conjunto conjunto, int cantidad){
+    TipoElemento clave;
+    int i = 0;
+    bool encontrado;
+    if(cantidad<=10000){
+      while (i < cantidad) {
+        clave=te_crear(0);
+        printf("Ingrese la clave: ");
+        scanf("%d", &clave->clave);
+        encontrado=cto_pertenece(conjunto,clave->clave);
+        if(!encontrado){
+            cto_agregar(conjunto, clave);
+            i++;  
+        }else{
+            printf("no puede ingresar elementos repetidos\n");
+        }
+        
+    }  
+    }else{
+        printf("no se puede cargar mas elementos que el tamaño maximo\n");
+    }
+    
+}
