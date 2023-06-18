ce685e435b86672753d153b3ad308da10b7c9072
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-06-18 17:57:07 -0300

    Add files via upload

---
+++ TPConjuntos/punto8.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include "punto8.h"
+#include "valida.h"
+#include "utilidadesConjuntos.h"
+#include "Tipo_Elemento.c"
+#include "validaciones.c"
+#include "utilidadesConjuntos.c"
+#include "conjuntos_avl.c"
+#include "listas_arreglos.c"
+#include "nodo.c"
+#include "arbol-avl.c"
+
+
+bool cto_son_iguales(Conjunto conj1,Conjunto conj2){
+    bool res=true;
+    int i=1;
+    TipoElemento x;
+    if(cto_cantidad_elementos(conj1)==cto_cantidad_elementos(conj2)){
+        while(res&&i<=cto_cantidad_elementos(conj1)){
+            x=cto_recuperar(conj1,i);
+        if(!cto_pertenece(conj2,x->clave)){
+            res=false;
+        }
+        i++;
+        }
+        return res;
+    }else return false;
+}
+
+int main(){
+    Conjunto conj1,conj2;
+    int clave;
+    bool res;
+    conj1= cto_crear();
+    printf("conjunto A:\n");
+    ingresarNumero("\ningrese la cantidad de elementos que quiera cargar: ",&clave);
+    cto_cargar_no_repetidos(conj1,clave);
+    conj2= cto_crear();
+    printf("\nconjunto B:\n");
+    ingresarNumero("\ningrese la cantidad de elementos que quiera cargar: ",&clave);
+    cto_cargar_no_repetidos(conj2,clave);
+    res=cto_son_iguales(conj1,conj2);
+    if(res){
+        printf("los conjuntos son iguales");
+    }else printf("los conjuntos son distintos");
+    printf("complejidad o(n^2)");
+}
---
+++ TPConjuntos/punto8.h
@@ -0,0 +1,7 @@
+#ifndef Punto8
+#define Punto8
+#include "conjuntos.h"
+
+bool cto_son_iguales(Conjunto conj1,Conjunto conj2);
+
+#endif 
